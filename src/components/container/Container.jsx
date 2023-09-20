0e3eb7c0b5c242d4dc5d4c4c01b86a56fbc8d804
aothymoon59 <mmoon8745@gmail.com>
2023-09-21 02:06:36 +0600

    initial setup done

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,9 @@
+const Container = ({ children }) => {
+    return (
+        <div className="max-w-[1320px] mx-auto px-4">
+            {children}
+        </div>
+    );
+};
+
+export default Container;
---
+++ src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import Container from "../../container/Container";
+
+const Footer = () => {
+    return (
+        <div>
+            <Container><h2>This is footer</h2></Container>
+
+        </div>
+    );
+};
+
+export default Footer;
